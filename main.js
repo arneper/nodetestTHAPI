d3a6439c0efdefa8ee384845e22d953e359268af
arneper <apheurberg@gmail.com>
2019-12-14 11:56:06 -0800

    added option for local machine

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 var http = require("http");
+var os = require('os');
+var PORT;
 
-const PORT = process.env.PORT || 5000;
+
+if (os.hostname().indexOf("local">-1)) {
+    const hostname= '127.0.0.1';
+    PORT = 3000;
+
+} else {
+    PORT = process.env.PORT || 5000;
+}
+
 
 http.createServer(function (request, response) {
    // Send the HTTP header 
@@ -11,8 +21,8 @@ http.createServer(function (request, response) {
    // Send the response body as "Hello World"
    response.end('Hello AP , how are you? \n');
 }).listen(PORT, () => {
-    console.log('Server running on ${PORT}/')
+    console.log('Server running on ${PORT}/');
 });
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:3000/');
