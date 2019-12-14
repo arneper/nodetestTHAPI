74c7f0d3ad99911d681553cf42199649e8e30d88
arneper <apheurberg@gmail.com>
2019-12-14 13:03:21 -0800

    moving along

--- main.js
+++ main.js
@@ -2,16 +2,12 @@ const http = require('http');
 var os = require('os');
 var PORT;
 
-console.log(os.hostname());
+//check if we are working locally or on heroku
 if (os.hostname().indexOf('local')>-1) {
     const hostname= '127.0.0.1';
     PORT = 3000;
-    console.log('this is where we ended up');
-
 } else {
     PORT = process.env.PORT || 5000;
- 
-    console.log (PORT);
 } 
 console.log (PORT);
 
@@ -26,6 +22,3 @@ http.createServer(function (request, response) {
 }).listen(PORT, () => {
     console.log(`Server running on ${PORT}/`);
 });
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:3000/');
