e95ff57fcab88b47933beaaaf0cf718248a41e42
arneper <apheurberg@gmail.com>
2019-12-14 18:30:08 -0800

    can we find it

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const addBook = (request, response) => {
 
   pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
     if (error) {
+        console.log("uggh")
       throw error
     }
     response.status(201).json({ status: 'success', message: 'Book added.' })
